448073335cfbdc88b4f16c2ef30367b80b607d27
ciaran <ciarancumiskey18594@gmail.com>
2019-07-23 11:18:12 +0100

    Implemented a ViewModelFactory and LiveData with ViewModel for ScoreFragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -45,9 +50,17 @@ class ScoreFragment : Fragment() {
                 container,
                 false
         )
-
-        // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        /** By passing in the Factory as a parameter, ViewModelProviders is told to use it to create
+         *  viewModel */
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(ScoreViewModel::class.java)
+        // Add Observer for score
+        viewModel.score.observe(this, Observer {newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        // Get args using by navArgs property delegate
         binding.scoreText.text = scoreFragmentArgs.score.toString()
         binding.playAgainButton.setOnClickListener { onPlayAgain() }
 
